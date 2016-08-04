63dc636aad2b248e7688f73307f73d01d3b1ece4
Sam O'Connor <samueljoeoconnor@hotmail.co.uk>
2016-08-04 09:41:35 +0100

    Added Solution

---
+++ RichPaste/Paste.cs
@@ -0,0 +1,73 @@
+﻿using Extensibility;
+using Microsoft.Office.Core;
+using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using Application = Microsoft.Office.Interop.OneNote.Application;
+
+namespace RichPaste
+{
+    [Guid("C06F8EC7-88DD-4D36-A24D-0EE91CB22B4E")]
+    [ProgId("RichPaste.Paste")]
+    [Serializable()]
+    public class Paste : IDTExtensibility2, IRibbonExtensibility
+    {
+        Application onApp = new Application();
+        
+
+        public IStream GetImage(string RTF)
+        {
+            MemoryStream mem = new MemoryStream();
+            Properties.Resources.RTF.Save(mem, ImageFormat.Png);
+            return new CCOMStreamWrapper(mem);
+
+        }
+       
+        public string GetCustomUI(string RibbonID)
+        {
+            return Properties.Resources.ribbon;
+        }
+
+        
+        public void OnAddInsUpdate(ref Array custom)
+        {
+        }
+
+        
+        public void OnBeginShutdown(ref Array custom)
+        {
+            if (onApp != null)
+            onApp = null;
+
+        }
+
+        
+        public void OnConnection(object application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom)
+        {
+            onApp = (Application)application;
+        }
+
+        
+        public void OnDisconnection(ext_DisconnectMode RemoveMode, ref Array custom)
+        {
+            onApp = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+        }
+
+       
+        public void OnStartupComplete(ref Array custom)
+        {
+        }
+
+        [STAThread]
+        public void PasteAction(IRibbonControl control)
+        {
+            Program.Main();
+        }
+
+    }
+}
---
+++ RichPaste/Program.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using Microsoft.Office.Core;
+using System.Threading;
+using Microsoft.Office.Interop.OneNote;
+using Application = Microsoft.Office.Interop.OneNote.Application;
+
+namespace RichPaste
+{
+    [Serializable()]
+    public class Program
+    {
+        [STAThread]
+        private static string ConvertRtfToXaml(string rtfText)
+        {
+            string result = null;
+
+            Exception threadEx = null;
+            Thread staThread = new Thread(
+            delegate ()
+            {
+                try
+                {
+                    var richTextBox = new System.Windows.Controls.RichTextBox();
+
+                    if (string.IsNullOrEmpty(rtfText)) result = ""; goto end;
+                    #pragma warning disable
+
+                    TextRange textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+
+                    //Create a MemoryStream of the Rtf content 
+
+                    using (var rtfMemoryStream = new MemoryStream())
+                    {
+                        using (var rtfStreamWriter = new StreamWriter(rtfMemoryStream))
+                        {
+                            rtfStreamWriter.Write(rtfText);
+                            rtfStreamWriter.Flush();
+                            rtfMemoryStream.Seek(0, SeekOrigin.Begin);
+
+                            //Load the MemoryStream into TextRange ranging from start to end of RichTextBox. 
+                            textRange.Load(rtfMemoryStream, DataFormats.Rtf);
+                        }
+                    }
+
+
+                    using (var rtfMemoryStream = new MemoryStream())
+                    {
+
+                        textRange = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+                        textRange.Save(rtfMemoryStream, "Xaml");
+                        rtfMemoryStream.Seek(0, SeekOrigin.Begin);
+                        using (var rtfStreamReader = new StreamReader(rtfMemoryStream))
+                        {
+                            result = rtfStreamReader.ReadToEnd();
+                        }
+
+                    }
+
+                    #pragma warning restore
+                    end:;
+                }
+                catch (Exception ex)
+                {
+                    threadEx = ex;
+                }
+            });
+            staThread.SetApartmentState(ApartmentState.STA);
+            staThread.Start();
+            staThread.Join();
+
+
+            return result;
+        }
+
+
+        
+
+        [STAThread]
+        public static string ConvertRtfToHtml(string rtfText)
+        {
+            string result = null;
+
+            Exception threadEx = null;
+            Thread staThread = new Thread(
+            delegate ()
+            {
+                try
+                {
+                    const string FlowDocumentFormat = "<FlowDocument>{0}</FlowDocument>";
+                    var xamlText = string.Format(FlowDocumentFormat, ConvertRtfToXaml(rtfText));
+
+                    result = HtmlFromXamlConverter.ConvertXamlToHtml(xamlText, false);
+                }
+                catch (Exception ex)
+                {
+                    threadEx = ex;
+                }
+            });
+            staThread.SetApartmentState(ApartmentState.STA);
+            staThread.Start();
+            staThread.Join();
+
+
+            return result;
+        }
+        
+        public void OnHierarchyChange(string bstrActivePageID)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public void OnNavigate()
+        {
+        }
+
+        //This [STAThread] is essential to access the clipboard
+        [STAThread] 
+        public static void Main()
+        {
+            
+            // Create New Instance of OneNote Application
+            var oneNote = new Application();
+            
+
+            //Get current active page (see commented for current NoteBook/Section)
+
+            /* string thisNoteBook = oneNote.Windows.CurrentWindow.CurrentNotebookId;
+               string thisSection  = oneNote.Windows.CurrentWindow.CurrentSectionId;    */
+            string thisPage = oneNote.Windows.CurrentWindow.CurrentPageId;
+            
+
+            //Get the content of the page
+            string xmlPage;
+            oneNote.GetPageContent(thisPage, out xmlPage);
+            
+
+            //Declarations for paste
+            string returnStringText = null;
+            string Pasteresult = null;
+
+
+            //Set Namespace (Can be set to dynamicly create here if needed)
+            string ns = @"one:";
+
+
+            //Find the end of the page and insert a new section for our content
+            int start = xmlPage.LastIndexOf("</one:Page>");
+            string startstr = xmlPage.Substring(0, start);
+            string endstr = xmlPage.Substring(start, xmlPage.Length - start);
+
+
+            //New section for our content (this is essential when the page is currently blank)
+            string outline =
+             "<" + ns + "Outline >" +
+             "<" + ns + "Position x=\"35.0\" y=\"60.0\"/>" + //This puts it just under title
+             "<" + ns + "Size width=\"750.75\" height=\"13.50\" isSetByUser=\"true\"/>" + //Makes the box nice and big
+             "<" + ns + "OEChildren>" +
+             "<" + ns + "OE>" +
+             "<" + ns + "T><![CDATA[]]></" + ns + "T>" +
+             "</" + ns + "OE>" +
+             "</" + ns + "OEChildren>" +
+             "</" + ns + "Outline>";
+
+
+            //Form the new page and replace our original page
+            string newpage = startstr + outline + endstr;
+            xmlPage = newpage;
+
+            //Get Text in Clipboard (needs a new STAThread)
+            string PastedText = null;
+            bool containsRTF = false;
+            bool containsHTML = false;
+            bool containsTEXT = false;
+            Exception threadEx = null;
+            Thread staThread = new Thread(
+                delegate ()
+                {
+                    try
+                    {
+                       containsRTF  = Clipboard.ContainsText(TextDataFormat.Rtf);
+                       containsHTML = Clipboard.ContainsText(TextDataFormat.Html);
+                       containsTEXT = Clipboard.ContainsText(TextDataFormat.Text);
+
+                        if(containsRTF == true && containsHTML == false)
+                        {
+                            PastedText = Clipboard.GetText(TextDataFormat.Rtf);
+                        }
+                        else if(containsHTML == true)
+                        {
+                            PastedText = Clipboard.GetText(TextDataFormat.Html);
+                        }
+                        else if(containsTEXT == true)
+                        {
+                            PastedText   = Clipboard.GetText(TextDataFormat.Text);
+                        }
+                       
+                    }
+
+                    catch (Exception ex)
+                    {
+                        threadEx = ex;
+                    }
+                });
+            staThread.SetApartmentState(ApartmentState.STA);
+            staThread.Start();
+            staThread.Join();
+
+           
+            //Does the clipboard contain Rich Text? (we put an extra && just to be sure it isn't html as they can sometimes get confused)
+            if (containsRTF == true && containsHTML == false)
+            {
+                //Get HTML from Rich Text
+                Pasteresult = ConvertRtfToHtml(PastedText);
+            }
+
+            //Does the clipboard contain HTML?
+            else if (containsHTML == true)
+            {
+
+                //Get HTML from Clipboard
+                returnStringText = PastedText;
+
+                //Get rid of everything before <body> or the first <span>
+                int pFrom = returnStringText.IndexOf("<BODY");
+                if (pFrom == -1) pFrom = returnStringText.IndexOf("<BODY");
+                if (pFrom == -1) pFrom = returnStringText.IndexOf("<SPAN");
+                if (pFrom == -1) pFrom = returnStringText.IndexOf("<span");
+                if (pFrom == -1) pFrom = returnStringText.IndexOf("<body");
+
+                //Get rid of everything after </body> or the last </span>
+                int pTo = returnStringText.LastIndexOf("</BODY>");
+                if (pTo == -1) pTo = returnStringText.LastIndexOf("</body>");
+                if (pTo == -1) pTo = returnStringText.LastIndexOf("</SPAN>");
+                if (pTo == -1) pTo = returnStringText.LastIndexOf("</span>");
+
+                Pasteresult = returnStringText.Substring(pFrom, pTo - pFrom + 7);
+            }
+
+            //Does the clipboard contain Plain Text?
+            else if (containsTEXT == true)
+            {
+                //Get the Text
+                Pasteresult = PastedText;
+
+                //Replace any "<" or ">" to ensure that it isn't read as HTML
+                Pasteresult = Pasteresult.Replace("<", "&lt;").Replace(">", "&gt;");
+
+                //Skip the block of HTML tag replacements
+                goto Skip;
+            }
+
+            //Remove all un-supported HTML tags (leaving pretty much just <span>'s)
+            //Also make sure the HTML tags that remain are all lower case or onenote will complain
+            Pasteresult = Pasteresult.Replace("<body", "<span").Replace("</body", "</span").Replace("<BODY", "<span").Replace("</BODY", "</span");
+            Pasteresult = Pasteresult.Replace("<div", "<span").Replace("</div", "</span").Replace("</LI", "</li");
+            Pasteresult = Pasteresult.Replace("<DIV", "<span").Replace("</DIV", "</span").Replace("<LI", "<li");
+            Pasteresult = Pasteresult.Replace("<SPAN", "<span").Replace("</SPAN", "</span").Replace("</DIV", "</span");
+            Pasteresult = Pasteresult.Replace("STYLE=", "style=").Replace("<UL", "<ul").Replace("</UL", "</ul");
+
+            //Do we have any paragraphs? If we do then we need to replace them with new XML CDATA sections
+            int containsPs = Regex.Matches(Pasteresult, "<[p,P]").Count;
+
+            //Get the ID of the last onenote block, then increment by 1
+            int LastIDNumberPosition = xmlPage.LastIndexOf("{B0}");
+            int LastIDNumberint = Convert.ToInt32(xmlPage.Substring(LastIDNumberPosition - 3, 2)) + 1;
+
+            //For each <p> 
+            while (containsPs > 0)
+            {
+                //Get a new GUID for the new block
+                Guid id = Guid.NewGuid();
+
+                //Get the incremented ID we created earlier
+                string LastIDNumber = Convert.ToString(LastIDNumberint);
+
+                //This is the text we will replace the <p>s with, it adds a new block 
+                string replace = " ]]></one:T></one:OE><one:OE authorResolutionID = \"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;+kg8hJunP8Va6ROK8UKusA==&quot;&gt;&lt;localId cid=&quot;40acfbd23ab1efa8&quot;/&gt;&lt;/resolutionId&gt;\" lastModifiedByResolutionID = \"&lt;resolutionId provider=&quot;Windows Live&quot; hash=&quot;+kg8hJunP8Va6ROK8UKusA==&quot;&gt;&lt;localId cid=&quot;40acfbd23ab1efa8&quot;/&gt;&lt;/resolutionId&gt;\" creationTime = \"2016-08-01T21:36:02.000Z\" lastModifiedTime = \"2016-08-01T21:36:02.000Z\" objectID = \"{" + id + "}{" + LastIDNumber + "}{B0}\" alignment = \"left\" quickStyleIndex = \"1\" style = \"font-family:Consolas;font-size:12.0pt\" ><one:T><![CDATA[<span";
+
+                //find the position of the <p>
+                int pos = Pasteresult.IndexOf("<P", StringComparison.CurrentCultureIgnoreCase);
+
+                //If there isn't any left then break
+                if (pos < 0) { break; }
+                else
+                {
+                    //Else replace the <p> with the new block
+                    Pasteresult = Pasteresult.Substring(0, pos) + replace + Pasteresult.Substring(pos + "<P".Length);
+                }
+
+                //Increment numbers
+                LastIDNumberint += 1;
+                containsPs -= 1;
+            }
+
+            //Remove the orphaned </p> tags
+            Pasteresult = Pasteresult.Replace("</P>", "").Replace("</p>", "");
+
+            //Plain text will now re-join here
+            Skip:
+            
+
+            //Get the position of the end of the last code block
+            int PageContent = xmlPage.LastIndexOf("]]></one:T>");
+            string startofPage = xmlPage.Substring(0, PageContent);
+            string endofPage = xmlPage.Substring(PageContent, xmlPage.Length - PageContent);
+
+            //Insert out pasted text into the current page
+            string NewPage = startofPage + Pasteresult + endofPage;
+            
+
+            try
+            {
+                //Update the onenote page
+                oneNote.UpdatePageContent(NewPage);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message + Environment.NewLine + Environment.NewLine + "Try using Ctrl + P", "Error Message");
+            }
+
+
+
+        }
+
+
+    }
+}
